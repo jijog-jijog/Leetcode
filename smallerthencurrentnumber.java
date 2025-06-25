4b9b99791ca6be5d10e3170751d43bc4f49ee352
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-25 22:38:37 +0530

    Add implementation of smallerthencurrentnumber class to count elements smaller than current

---
+++ smallerthencurrentnumber.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+public class smallerthencurrentnumber {
+    public static void main(String[] args) {
+        int [] nums = {7,7,7,7};
+        int [] arr = new int[nums.length];
+
+    
+        for(int i =0; i<nums.length; i++){
+            int count = 0;
+            for(int j=0; j<nums.length; j++){
+                
+                if ((nums[j] < nums[i])) {
+                    count = count+1;
+                    arr[i] = count;
+                }
+            }
+        }
+    for(int ar : arr){
+        System.out.println(ar);
+    }
+    }
+}
