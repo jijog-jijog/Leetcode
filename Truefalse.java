d37986b690290295ff39dcf56537516631d54e7b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-25 19:37:16 +0530

    Add Truefalse class to evaluate if each child can have the greatest number of candies

---
+++ Truefalse.java
@@ -0,0 +1,28 @@
+public class Truefalse {
+    public static void main(String[] args) {
+        int [] candies = {2,3,5,1,3};
+        int [] newarr = new int[candies.length];
+        boolean [] setarr = new boolean[candies.length];
+        int extraCandies = 10;
+
+        for(int i = 0; i < candies.length; i++ ){
+            int max = candies[i] + extraCandies;
+            newarr[i] = max;
+        }
+        
+        for(int i = 0; i<=candies.length-1; i++){
+            for(int j =0; j<=candies.length-1; j++){
+                  if (newarr[i] < newarr[j]) {
+                    setarr[j] = true;
+                  }
+            }
+            }
+                    
+        
+       
+    
+    for(boolean arr : setarr){
+        System.out.println(arr);
+    }
+}
+}
