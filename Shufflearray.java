fd85b71846f25972ffad12330cefa90b4a600906
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-25 17:38:12 +0530

    Add implementations for Shufflearray and suffle classes

---
+++ Shufflearray.java
@@ -0,0 +1,28 @@
+public class Shufflearray {
+    public static void main(String[] args) {
+
+        //code is wrong
+        
+        int [] nums = {1,2,3,4,5,6};
+        int [] val1 = new int[nums.length/2];
+        int [] val2 = new int[nums.length/2];
+        int [] ans = new int[nums.length];
+
+        for(int i = 0; i< nums.length/2; i++){
+               val1[i] = nums[i];
+        }
+       
+        for(int j = nums.length/2; j<nums.length; j++ ){
+            val2[j] = nums[j];
+        }
+        for(int i =0; i<val1.length; i++){
+            val1[i] = ans[i*2+1];
+            // val2[i] = ans[i];
+
+        }
+        
+        for(int arr : ans){
+            System.out.println(arr);
+        }
+    }
+}
---
+++ suffle.java
@@ -0,0 +1,20 @@
+public class suffle {
+    public static void main(String[] args) {
+         int [] nums = {1,2,3,4,5,6};
+         int n = 3;
+         int[] ans = new int[nums.length];
+
+         for(int i = 0; i<n; i++){
+            ans[i*2] = nums[i];
+            ans[i*2+1] = nums[n+i];
+         }
+
+         for(int arr : ans){
+            System.out.println(arr);
+         }
+        
+
+        
+
+    }
+}
