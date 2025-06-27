e66e2f93d9b2fd90eabc99184cfc4e0daba3314f
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-27 16:25:02 +0530

    Refactor flipimage class to improve image initialization and enhance flipping logic

--- flipimage.java
+++ flipimage.java
@@ -1,24 +1,27 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class flipimage {
     public static void main(String[] args) {
-        int [][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        int [][] image = {{1,1,0},
+                          {1,0,1},
+                          {1,0,0}};
+        int [][] fliped = new int[image.length][];
 
-        for(int i =0; i<image.length; i++){
-            for(int j= image[i].length-1; j>=0; j--){
-                System.out.println(Arrays.toString(image[j]));
-            }
+       for(int i=0; i<image.length; i++){
+           fliped[i] = new int[image[i].length];
+        for(int l =0, r=image[i].length-1; l <= r; l++, r--){  //not int for the second varibale and ;<-- this symbol plus , comma
+         
+            int left = image[i][l] ^ 1;
+            int right = image[i][r] ^ 1;
+
+            fliped[i][r] = left;
+            fliped[i][l] = right;
         }
-
-
-        // for(int i = image.length - 1; i >= 0; i--){
-        //      System.out.println(Arrays.toString(image[i])); 
-        // }
-
-
-    //     for(int[] a : image){
-    //        System.out.println(Arrays.toString(a)); 
-    //     }
+       }
+    for(int a[] : fliped){
+        System.err.println(Arrays.toString(a));
+    }
     }
     
 }
