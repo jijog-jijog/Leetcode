b9758869249d4c705d88b2a7c971c214b00833d2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-28 11:25:29 +0530

    Remove unnecessary imports and whitespace in oddvalueopti class for cleaner code

--- oddvalueopti.java
+++ oddvalueopti.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
-
 public class oddvalueopti {
-    public static void main(String[] args) {
-        
-          int m = 2;
+    public static void main(String[] args) {  
+        int m = 2;
         int n = 3;
         int[][] indices = {
             {0, 1},
             {1, 1}
         };
-
         int [] row = new int[m];
         int [] column = new int[n];
 
@@ -27,6 +23,5 @@ public class oddvalueopti {
             }
         }
     System.out.println(count);
-
 }
 }
