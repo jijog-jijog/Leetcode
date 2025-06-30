7b6b274624065a52caed6faf823aeb2b526c32d8
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-30 12:10:29 +0530

    Implement count of numbers with even digit counts in main method

--- evennumberdigits1295.java
+++ evennumberdigits1295.java
@@ -2,5 +2,14 @@ public class evennumberdigits1295 {
     public static void main(String[] args) {
         int [] nums = {12,354,2,6,7896};
 
+        int count = 0;
+        for(int a : nums){
+            int digitcount = String.valueOf(Math.abs(a)).length();
+            if(digitcount % 2 == 0){
+                count++;
+            }
+        }
+    System.out.println(count);
+
     }
 }
