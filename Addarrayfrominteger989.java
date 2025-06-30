c938033eaf7a5710daf8f1813510dd7f6911feb9
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-30 17:33:09 +0530

    Add initial implementation of Addarrayfrominteger989 class to sum array elements with a constant

---
+++ Addarrayfrominteger989.java
@@ -0,0 +1,15 @@
+public class Addarrayfrominteger989 {
+    public static void main(String[] args) {
+        int[] num = {1,2,0,0};
+        int [] out = new int[num.length];
+        int k = 34;
+
+        int count = 0;
+        for(int a : num){
+            count = count + a;
+        }
+        int result = count+k;
+        System.out.println(result);
+    }
+
+}
