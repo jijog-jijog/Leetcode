1e01a128815a2b3195fe0d5e9e7d1a478bae890f
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-30 16:16:28 +0530

    Update nums array and fix logic for counting even digit numbers in evendigit1295opti class

--- evendigit1295opti.java
+++ evendigit1295opti.java
@@ -1,25 +1,18 @@
 public class evendigit1295opti {
     public static void main(String[] args) {
-        int [] nums = {555,901,482,1771};
+        int [] nums = {12,345,2,6,7896};
 
         int count = 0;
+         int value = 0;
         for(int a : nums){
-            // int digitcount = String.valueOf(Math.abs(a)).length();
-
-            int value = 0;
             while ((a<0)) {
                 value++;
                 a = a/10;
             }
-
-            if ((value % 2 == 0)) {
+        }
+         if ((value % 2 == 0)) {
                 count++;
             }
-
-            // if(digitcount % 2 == 0){
-            //     count++;
-            // }
-        }
     System.out.println(count);
 
     }
