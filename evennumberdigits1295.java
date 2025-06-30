49de62c4c6fce5b7a74422a7f14166063bab2290
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-30 12:11:17 +0530

    Update nums array in main method for even digit count calculation

--- evennumberdigits1295.java
+++ evennumberdigits1295.java
@@ -1,6 +1,6 @@
 public class evennumberdigits1295 {
     public static void main(String[] args) {
-        int [] nums = {12,354,2,6,7896};
+        int [] nums = {555,901,482,1771};
 
         int count = 0;
         for(int a : nums){
