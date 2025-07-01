1bc1d7844cf0f25f99f5ed2ce547a722fb220ccc
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-01 19:32:10 +0530

    Fix matrix initialization and update check method signature to include target parameter

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -1,18 +1,19 @@
 public class marix90rotation1886 {
     public static void main(String[] args) {
-        int [][] mat = {{0,1},{1,0}};
+        int [][] mat = {{1},{1,0}};
         int [][] target = {{0,1},{1,0}};
         int[][] flip = new int[mat.length][mat.length];
-        boolean res = false;
-         if (mat.length != target.length || mat[0].length != target.length){
-            System.out.println(true);
-         }
-        boolean result = check(mat, target);
+        
+        boolean result = check(mat, target, target);
         System.out.println(result); 
         }
-        public static boolean check(int[][] mat , int [][]flip){
+
+        public static boolean check(int[][] mat , int [][]flip , int[][]target){
         int n = mat.length;
-       
+        boolean res = true;
+        for(int i =0)
+        res = (mat.length != target.length || mat[0].length != target.length) ? false : res;
+           break;
 
         for(int i =0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
