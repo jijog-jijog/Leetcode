0f4cab7fa5fd345f694616517cd8fe51f05764d4
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-01 22:57:28 +0530

    Refactor check method in marix90rotation1886 class to improve readability by removing unnecessary comments and ensuring consistent formatting

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -7,27 +7,27 @@ public class marix90rotation1886 {
         boolean result = check(mat, target, target);
         System.out.println(result); 
         }
-
         public static boolean check(int[][] mat , int [][]flip , int[][]target){
         int n = mat.length;
         boolean res = true;
         for(int i =0; i<4; i++){
-            res = (mat.length != target.length || mat[0].length != target.length) ? false : res;
-           break;
+        res = (mat.length != target.length || mat[0].length != target.length) ? false : res;
+        break;
 
-            for(int k =0; k<mat.length; k++){
-            for(int j=0; j<mat[i].length; j++){
-                  System.out.println();
-            }
-            }
+        // for(int k =0; k<mat.length; k++){
+        // for(int j=0; j<mat[i].length; j++){
+        //           System.out.println();
+        //     }
+        //     }
         }
         
-    
+    return res;
         
     }      
   
+        
     }
 
-}
+
     
 
