f37beea1813058f83c2fb4c42508c2116323c616
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-01 19:26:26 +0530

    Refactor check method in marix90rotation1886 class to use flip parameter and improve matrix dimension validation

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -2,12 +2,17 @@ public class marix90rotation1886 {
     public static void main(String[] args) {
         int [][] mat = {{0,1},{1,0}};
         int [][] target = {{0,1},{1,0}};
+        int[][] flip = new int[mat.length][mat.length];
+        boolean res = false;
+         if (mat.length != target.length || mat[0].length != target.length){
+            System.out.println(true);
+         }
         boolean result = check(mat, target);
         System.out.println(result); 
         }
-        public static boolean check(int[][] mat , int [][]target){
+        public static boolean check(int[][] mat , int [][]flip){
         int n = mat.length;
-        if (mat.length != target.length || mat[0].length != target.length) return false;
+       
 
         for(int i =0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
