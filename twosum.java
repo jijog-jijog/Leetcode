e6ebe3759ecbaee557eace001669183f26f4bbab
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 14:37:17 +0530

    Add initial implementation of two-sum solution with input array and target

---
+++ twosum.java
@@ -0,0 +1,10 @@
+public class twosum {
+    public static void main(String[] args) {
+        
+        int [] nums = {2,7,11,15};
+        int [] res = new int[nums.length];
+        int target = 9;
+
+
+    }
+}
