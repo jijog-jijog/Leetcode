ccb67eb167cbdac6b03543bea238634abb34e9c6
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 14:50:26 +0530

    Refactor two-sum implementation for improved readability and efficiency

--- twosum.java
+++ twosum.java
@@ -1,21 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class twosum {
     public static void main(String[] args) {
         
         int [] nums = {2,7,11,15};
-        int [] res = new int[nums.length];
+        List <Integer> set = new ArrayList<>();
         int target = 9;
         
         for(int i=0; i<nums.length; i++){
            for(int j=0; j<nums.length; j++){
             if (nums[i]+nums[j] == target) {
-                    res[0] = i;
-                    res[0] = j;
+                  set.add(i);
+                  set.add(j);
                 }
            }
         }
-    for(int a : res){
+    for(int a : set){
         System.out.println(a);
     }
     }
