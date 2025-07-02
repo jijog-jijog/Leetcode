f13bb6b0ad483ceef89951411e7c6925f3c73812
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 12:09:40 +0530

    Initial commit of matrix rotation implementation

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -5,6 +5,8 @@ public class marix90rotation1886 {
        int[][] mat = { { 0,1 }, { 1, 0 } };
         int[][] target = { { 1,0 }, { 0,1} };
         int[][] flip = new int[mat.length][mat.length];
+
+        public
         
 
     }
