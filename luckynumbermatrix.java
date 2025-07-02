805389f3f18f6650cf5ea9ad57e137830a5eaeb1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 16:31:34 +0530

    Add loop structure for iterating through the 2D array in luckynumbermatrix

--- luckynumbermatrix.java
+++ luckynumbermatrix.java
@@ -3,5 +3,8 @@ public class luckynumbermatrix {
         int[][] set = {{1,10,4,2},
                        {9,3,8,7},
                        {15,16,17,12}};
+        for(int i=0; i<set.length; i++){
+            
+        }
     }
 }
