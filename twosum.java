5705d8100c6837d4e320b36e9fc812686ebf6fd4
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 14:40:59 +0530

    Add nested loop structure for two-sum implementation

--- twosum.java
+++ twosum.java
@@ -6,7 +6,9 @@ public class twosum {
         int target = 9;
         
         for(int i=0; i<nums.length; i++){
-           
+           for(int j=0; j<nums.length; j++){
+            
+           }
         }
     }
 }
