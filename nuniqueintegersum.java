9d203308eb85c121bb2084d8caf8c729b10b9d49
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 15:51:02 +0530

    Fix array initialization and loop boundaries in nuniqueintegersum main method

--- nuniqueintegersum.java
+++ nuniqueintegersum.java
@@ -1,12 +1,15 @@
 public class nuniqueintegersum {
     public static void main(String[] args) {
         int n = 5;
-        int[] set = new int[n+1];
-        for(int i=0; i<=n+1; i++){
-            for(int j=n/2+1; j<=n+1; j++){
+        int[] set = new int[n];
+        for(int i=0; i<=n; i++){
+            for(int j=n/2+1; j<=n; j++){
                 set[i] = i+1;
                 set[j] = j+1;
             }
         }
+    for(int a : set){
+        System.out.println(a);
+    }
     }
 }
