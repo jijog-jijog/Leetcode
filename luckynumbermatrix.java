3253102617e42d0f545b6c07a1cae3ac9070c956
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 17:17:57 +0530

    Refactor logic to correctly find minimum values in each row and store them in a list for output in luckynumbermatrix

--- luckynumbermatrix.java
+++ luckynumbermatrix.java
@@ -1,32 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class luckynumbermatrix {
     public static void main(String[] args) {
+        List<Integer> ans = new ArrayList<>();
         int[][] set = {{1,10,4,2},
                        {9,3,8,7},
-                       {15,16,17,12}};
+                       {15,16,17,1}};
         for(int i=0; i<set.length; i++){
 
-            int min =set[0][0];
+            int min =set[i][0];
             int col = 0;
-            for(int j=0; j<set.length; j++){
-                for(int k =0; k<set[j].length; k++){
-                    if (set[j][k] < set[0][0]) {
-                        set[j][k] = min;
-                        col = k;
-                    }
+            for(int j=1; j<set[i].length; j++){
+                    if (set[i][j] < min) {
+                        min = set[i][j];
+                        col = j;
                 }
             }
             boolean val = true;
             for(int l=0; l<set.length; l++){
-                if (min > set[l][col]) {
+                if ( set[l][col] > min) {
                     val = false;
                     break;
                 }
                 }
-            if (val) {
-                System.out.println(min);
-                break;
+            if (val == true) {
+                 ans.add(min);
             }
             }
+        for(int a : ans){
+            System.out.println(a);
+        }
         }
     }
 
