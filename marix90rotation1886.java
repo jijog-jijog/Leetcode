a471e3b12cf0745474481e10578aac88049b53dd
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 12:15:35 +0530

    Add check method and result output in matrix rotation implementation

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -6,7 +6,13 @@ public class marix90rotation1886 {
         int[][] target = { { 1,0 }, { 0,1} };
         int[][] flip = new int[mat.length][mat.length];
 
-        public
+            boolean result = check(mat, target, flip);
+            System.out.println(result);
+        }
+    
+        public static boolean check(int[][] mat, int[][] target, int[][] flip){
+            // Implement your logic here
+            return false;
         
 
     }
