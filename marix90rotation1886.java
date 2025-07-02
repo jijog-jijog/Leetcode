e9baf4c52a83d3be8eeb2871863def82c19edae1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 09:53:53 +0530

    Update initial matrix values in main method for correct test case

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class marix90rotation1886 {
     public static void main(String[] args) {
-        int[][] mat = { { 0,1 }, { 1, 0 } };
+        int[][] mat = { { 1,1 }, { 1, 0 } };
         int[][] target = { { 0, 1 }, { 1, 0 } };
         int[][] flip = new int[mat.length][mat.length];
 
