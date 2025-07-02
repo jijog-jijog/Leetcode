9ceafb2213d18ae888fed0b4b657c027fcad227a
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 14:45:29 +0530

    Implement logic to check for target sum in nested loop of two-sum solution

--- twosum.java
+++ twosum.java
@@ -7,7 +7,10 @@ public class twosum {
         
         for(int i=0; i<nums.length; i++){
            for(int j=0; j<nums.length; j++){
-            
+            if (nums[i]+nums[j] == target) {
+                    res[0] = i;
+                    res[0] = j;
+                }
            }
         }
     }
