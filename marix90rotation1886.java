bca56f6e2459960e5ddd1c921baef2e4570079db
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 09:50:58 +0530

    Refactor check method in marix90rotation1886 class for improved readability and structure

--- marix90rotation1886.java
+++ marix90rotation1886.java
@@ -2,48 +2,54 @@ import java.util.Arrays;
 
 public class marix90rotation1886 {
     public static void main(String[] args) {
-        int [][] mat = {{1,0},{1,0}};
-        int [][] target = {{0,1},{1,0}};
+        int[][] mat = { { 0,1 }, { 1, 0 } };
+        int[][] target = { { 0, 1 }, { 1, 0 } };
         int[][] flip = new int[mat.length][mat.length];
-        
-        boolean result = check(mat, target,flip);
-        System.out.println(result); 
-        for(int a[] : flip){
+
+        boolean result = check(mat, target, flip);
+        System.out.println(result);
+        for (int a[] : mat) {
             System.out.println(Arrays.toString(a));
-        }
+            }
+    
+    
         }
         public static boolean check(int[][] mat , int [][]flip , int[][]target){
         int n = mat.length;
         boolean res = true;
-        for(int i =0; i<4; i++){
 
-            for(int x= 0; x<mat.length; x++){
+         for(int i =0; i<4; i++){
+
+            
+            if (mat.length != target.length || mat[0].length != target.length) {
+            return false;
+            }
+
+             for(int x= 0; x<mat.length; x++){
                 for(int y=0; y<mat[x].length; y++){
-                    
+
+                    if (mat[x][y] == target[x][y]) {
+                        // return true;
+
+                    }else{
+
+                         for(int k =0; k<n; k++){
+                         for(int j=0; j<mat[k].length; j++){
+                         flip[j][n - 1 -k] = mat[k][j];
+                         }
+                        }
+                    flip = mat;
+                    }
                 }
             }
-
-        if (mat.length != target.length || mat[0].length != target.length) {
-            res = false;
-            
-            }else{
-                res = true;
-
-        for(int k =0; k<n; k++){
-        for(int j=0; j<mat[k].length; j++){
-        flip[j][n - 1 -k] = mat[k][j];
-        flip = mat;
-            }
-            }
+        
+             
         }
-        
-    }
-    return res;         
-        
-    }
-}
-
-
-
+       
+    return true;
     
-
+    }
+        
+       
+  
+}
