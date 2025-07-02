5f0c0ade26a52ff4c910e4eb87420344ce1cab62
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 15:51:39 +0530

    Fix loop boundaries in nuniqueintegersum main method and update value assignment in the inner loop

--- nuniqueintegersum.java
+++ nuniqueintegersum.java
@@ -2,10 +2,10 @@ public class nuniqueintegersum {
     public static void main(String[] args) {
         int n = 5;
         int[] set = new int[n];
-        for(int i=0; i<=n; i++){
-            for(int j=n/2+1; j<=n; j++){
+        for(int i=0; i<n; i++){
+            for(int j=n/2+1; j<n; j++){
                 set[i] = i+1;
-                set[j] = j+1;
+                set[j] = j+5;
             }
         }
     for(int a : set){
