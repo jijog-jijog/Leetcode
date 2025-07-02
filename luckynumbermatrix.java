d724ee29e2ecaf5db30fe1f97085d27dcb8aa933
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 16:30:43 +0530

    Add initial implementation of luckynumbermatrix class with main method and sample 2D array

---
+++ luckynumbermatrix.java
@@ -0,0 +1,7 @@
+public class luckynumbermatrix {
+    public static void main(String[] args) {
+        int[][] set = {{1,10,4,2},
+                       {9,3,8,7},
+                       {15,16,17,12}};
+    }
+}
