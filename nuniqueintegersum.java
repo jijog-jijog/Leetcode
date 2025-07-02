383060d330d58b06a5c053b27686a93bf3233c49
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-02 15:45:40 +0530

    Update loop boundaries in nuniqueintegersum main method

--- nuniqueintegersum.java
+++ nuniqueintegersum.java
@@ -1,9 +1,9 @@
 public class nuniqueintegersum {
     public static void main(String[] args) {
         int n = 5;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<=n+1; i++){
             for(int j=n/2+1; j<=n+1; j++){
-                
+
             }
         }
     }
