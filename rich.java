61dd95a23fdc17a1813d42ec4a28d016486e1d5d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-13 10:04:04 +0530

    Add initial implementation of rich class to print elements of a 2D array

---
+++ rich.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class rich {
+    public static void main(String[] args) {
+        int[][] nums = {{1,2,3},{3,2,1}};
+
+        for(int[] a : nums){
+
+            for(int b : a){
+                  System.out.println(b);
+            }
+          
+        }
+    }
+}
