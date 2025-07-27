e02598a4909c0d4748c7af017ee41b4a1d4bac17
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-27 22:28:34 +0530

    g

--- removedupli.java
+++ removedupli.java
@@ -12,8 +12,9 @@ class removedupli{
         int i =0;
 
         for(int j=1; j<nums.length; j++){
-            
+
             if (nums[i] != nums[j]) {
+                
                 i++;
                 nums[i] = nums[j];
             }
