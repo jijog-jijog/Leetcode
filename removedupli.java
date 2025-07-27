c8be99b83a8d0e2e844ab9f7e77c5dcda3a7001d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-27 22:28:16 +0530

    g

--- removedupli.java
+++ removedupli.java
@@ -9,9 +9,7 @@ class removedupli{
     public static void main(String[] args) {
         // int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int[] nums = {0,0,1,1,1,2,2,3,3,4,4,4,4,5};
-        
         int i =0;
-
         for(int j=1; j<nums.length; j++){
             if (nums[i] != nums[j]) {
                 i++;
