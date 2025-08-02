5be2b730c0c711b9e17e0efab5a8e91a24182eee
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-02 22:46:58 +0530

    k

--- rich.java
+++ rich.java
@@ -1,6 +1,7 @@
 
 public class rich {
     public static void main(String[] args) {
+        
         int[][] nums = {{1,2,3},{3,2,1}};   
         for(int[] a : nums){
             for(int b : a){
