60a6610093a66b4a2fb96c4d34b2bb365c4ae151
Jijo G <145054528+jijog-jijog@users.noreply.github.com>
2025-08-03 23:41:34 +0530

    Update Richestcustomer.java

--- Richestcustomer.java
+++ Richestcustomer.java
@@ -1,8 +1,7 @@
 import java.util.Arrays;
 
 public class Richestcustomer {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {      
         int [] [] accounts = {{1,3,5,6,8,2,9},{4,9,7,5,3},{3,4,9,7,6}};
 
         int maxval = 0;
