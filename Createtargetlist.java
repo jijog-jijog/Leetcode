bedbd30e4a3bf127cc928b21e0cf03160e04e998
Jijo G <145054528+jijog-jijog@users.noreply.github.com>
2025-08-04 22:50:50 +0530

    Update Createtargetlist.java

--- Createtargetlist.java
+++ Createtargetlist.java
@@ -6,7 +6,6 @@ public class Createtargetlist {
         int [] index = {0,1,2,2,1};
         int [] arr = new int[nums.length];
         ArrayList<Integer> list = new ArrayList<>();
-
         for(int i=0; i<nums.length; i++){
             list.add(index[i], nums[i]);
         };
